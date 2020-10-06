a140039289e5b5f09d87b98ec2c7588495a17416
Yannick Lang <layaxx@gmx.de>
2020-10-06 10:34:26 +0200

    refactor tests

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,8 @@
-import unittest
-import os,sys
-from numpy.testing._private.utils import assert_equal
-
-from pandas.core.frame import DataFrame
+import os,sys, unittest
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import equipment
-import pandas
+import pandas, decimal
 import numpy as np
-import decimal
 
 class LoadDataTestCase(unittest.TestCase):
 
@@ -17,7 +12,7 @@ class LoadDataTestCase(unittest.TestCase):
     def tearDown(self):
         self.dataframe = None
 
-    def testReadCSV(self):
+    def test_read_csv(self):
         actual = self.dataframe
         expected = pandas.DataFrame(np.array([[1,1,"Produkt1","Medienraum","Stahlschrank","","Software","Magix","","","","unklar ob auf PC / welchem PC"],
                     [2,1,"Produkt2","Medienraum","Stahlschrank","","Zubehör","Coast",decimal.Decimal("27.90"),"GHW","",""],
@@ -26,91 +21,163 @@ class LoadDataTestCase(unittest.TestCase):
 
         pandas.testing.assert_frame_equal(actual, expected, check_dtype=False)
 
-    def testConvertIndexToInteger(self):
-        assert_equal(type(self.dataframe["Index"][1]), np.int64)
+    def test_convert_index_to_integer(self):
+        """
+        Test if values in Index column are converted to Integer during loading
+        """
+        self.assertEqual(type(self.dataframe["Index"][1]), np.int64)
 
-    def testConvertMengeToInteger(self):
-        assert_equal(type(self.dataframe["Menge"][1]), np.int64)
+    def test_convert_menge_to_integer(self):
+        """
+        Test that values in Menge column are converted to Integer during loading
+        """
+        self.assertEqual(type(self.dataframe["Menge"][1]), np.int64)
 
-    def testConvertPreisToDecimal(self):
-        assert_equal(type(self.dataframe["Preis"][1]), decimal.Decimal)
+    def test_convert_preis_to_decimal(self):
+        """
+        Test that values in Preis column are converted to Decimal during loading
+        """
+        self.assertEqual(type(self.dataframe["Preis"][1]), decimal.Decimal)
 
-    def testFormatPreis(self):
-        assert_equal(len(str(self.dataframe["Preis"][1])), 5)
+    def test_format_preis(self):
+        """
+        Test that values in preis column have correct formatting
+        """
+        self.assertEqual(len(str(self.dataframe["Preis"][1])), 5)
 
 class FormatPriceTestCase(unittest.TestCase):
 
-    def testReturnEmptyStringForNan(self):
+    def test_return_empty_string_for_nan(self):
+        """
+        Test that format_price() returns empty String for NaN Input
+        """
         actual = equipment.format_price(np.nan)
-        assert_equal(actual, "")
+        self.assertEqual(actual, "", )
 
-    def testConvertFloatToDecimal(self):
+    def test_convert_float_to_decimal(self):
+        """
+        Test that format_price() returns correct Decimal for float input 
+        """
         actual = equipment.format_price(9.85)
-        assert_equal(actual, decimal.Decimal.from_float(9.85))
+        self.assertEqual(actual, decimal.Decimal.from_float(9.85))
 
-    def testConvertStringWithPointToDecimal(self):
+    def test_convert_string_with_point_to_decimal(self):
+        """
+        Test that format_price() returns correct Decimal for String input with point as Decimal Seperator
+        """
         actual = equipment.format_price("9.85")
-        assert_equal(actual, decimal.Decimal("9.85"))
+        self.assertEqual(actual, decimal.Decimal("9.85"))
 
-    def testConvertStringWithCommaToDecimal(self):
+    def test_convert_string_with_comma_to_decimal(self):
+        """
+        Test that format_price() returns correct Decimal for String Input with Comma as Decimal Seperator
+        """
         actual = equipment.format_price("9,85")
-        assert_equal(actual, decimal.Decimal("9.85"))
+        self.assertEqual(actual, decimal.Decimal("9.85"))
 
-    def testPadPrice(self):
+    def test_pad_price_intlike(self):
+        """
+        Test that format_price() pads Output with Zeros for Integer-like String Input
+        """
         actual = equipment.format_price("9")
-        assert_equal(actual, decimal.Decimal("9.00"))
+        self.assertEqual(actual, decimal.Decimal("9.00"))
 
-    def testPadPrice2(self):
+    def test_pad_price_floatlike(self):
+        """
+        Test that format_price() pads Output with Zeros for Float-like String Input
+        """
         actual = equipment.format_price("9,8")
-        assert_equal(actual, decimal.Decimal("9.80"))
+        self.assertEqual(actual, decimal.Decimal("9.80"))
 
-    def testPadPrice3(self):
+    def test_cut_off_price(self):
+        """
+        Test that format_price() cuts Number of after 2 Decimal Places for String Input with more than 2 Decimal Places
+        """
         actual = equipment.format_price("8.9999")
-        assert_equal(actual, decimal.Decimal("8.99"))
+        self.assertEqual(actual, decimal.Decimal("8.99"))
     
 
 class EscapeSpecialCharactersCase(unittest.TestCase):
 
-    def testReturnUnmodifiedInputIfNoCharacterNeedsToBeEscaped(self):
+    def test_return_unmodified_input_if_no_character_needs_to_be_escaped(self):
+        """
+        Test that escape_special_charcters() does not change string if it does not contain characters that need to be escaped in LaTeX
+        """
         string = r"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXJZ0123456789!§-/()=?´`+*'ß,.:;°"
         result = equipment.escape_special_characters(string)
-        assert_equal(string, result)
+        self.assertEqual(string, result)
 
-    # escapes all occurrences of # $ % & ~ _ ^ \ { } and escapes them in order to make them safe for latex compiling
+    def test_escape_hashtag(self):
+        """
+        Test that escape_special_charcters() does escape Hashtag Symbol ("#")
+        """
+        self.assertEqual(equipment.escape_special_characters("#"), r"\#")
 
-    def testEscapeHashtag(self):
-        assert_equal(equipment.escape_special_characters("#"), r"\#")
+    def test_escape_dollar_sign(self):
+        """
+        Test that escape_special_charcters() does escape Dollar Sign ("$")
+        """
+        self.assertEqual(equipment.escape_special_characters("$"), r"\$")
 
-    def testEscapeDollarSign(self):
-        assert_equal(equipment.escape_special_characters("$"), r"\$")
+    def test_escape_percentage(self):
+        """
+        Test that escape_special_charcters() does escape Percentage Sign ("%")
+        """
+        self.assertEqual(equipment.escape_special_characters("%"), r"\%")
 
-    def testEscapePercentage(self):
-        assert_equal(equipment.escape_special_characters("%"), r"\%")
+    def test_escape_and(self):
+        """
+        Test that escape_special_charcters() does escape "&"-Symbol
+        """
+        self.assertEqual(equipment.escape_special_characters("&"), r"\&")
 
-    def testEscapeAnd(self):
-        assert_equal(equipment.escape_special_characters("&"), r"\&")
+    def test_escape_tilde(self):
+        """
+        Test that escape_special_charcters() does escape Tilde ("~")
+        """
+        self.assertEqual(equipment.escape_special_characters("~"), r"\textasciitilde")
 
-    def testEscapeTilde(self):
-        assert_equal(equipment.escape_special_characters("~"), r"\textasciitilde")
+    def test_escape_underscore(self):
+        """
+        Test that escape_special_charcters() does escape underscore ("_")
+        """
+        self.assertEqual(equipment.escape_special_characters("_"), r"\_")
 
-    def testEscapeUnderscore(self):
-        assert_equal(equipment.escape_special_characters("_"), r"\_")
+    def test_escape_circum(self):
+        """
+        Test that escape_special_charcters() does escape Circum ("^")
+        """
+        self.assertEqual(equipment.escape_special_characters("^"), r"\textasciicircum")
 
-    def testEscapeCircum(self):
-        assert_equal(equipment.escape_special_characters("^"), r"\textasciicircum")
+    def test_escape_backslash(self):
+        """
+        Test that escape_special_charcters() does escape Backslash ("\")
+        """
+        self.assertEqual(equipment.escape_special_characters("\\"), r"\textbackslash")
 
-    def testEscapeBackslash(self):
-        assert_equal(equipment.escape_special_characters("\\"), r"\textbackslash")
+    def test_escape_opening_braces(self):
+        """
+        Test that escape_special_charcters() does escape Tilde ("{")
+        """
+        self.assertEqual(equipment.escape_special_characters("{"), r"\{")
 
-    def testEscapeOpeningBraces(self):
-        assert_equal(equipment.escape_special_characters("{"), r"\{")
+    def test_escape_closing_braces(self):
+        """
+        Test that escape_special_charcters() does escape Tilde ("}")
+        """
+        self.assertEqual(equipment.escape_special_characters("}"), r"\}")
 
-    def testEscapeClosingBraces(self):
-        assert_equal(equipment.escape_special_characters("}"), r"\}")
+    def test_replace_empty_strings_with_na(self):
+        """
+        Test that escape_special_charcters() does replace empty Strings with "n/a"
+        """
+        self.assertEqual(equipment.escape_special_characters(""), r"n/a")
 
-    def testreplaceEmptyStringsWithNA(self):
-        assert_equal(equipment.escape_special_characters(""), r"n/a")
-
+    def test_escape_multiple_occurrences(self):
+        """
+        Test that escape_special_charcters() does escape and symbol ("&") even if its found in the input multiple times 
+        """
+        self.assertEqual(equipment.escape_special_characters("test##test#"), r"test\#\#test\#")
 
 if __name__ == "__main__":
     unittest.main() # run all tests
