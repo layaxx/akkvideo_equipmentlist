49f4f3459cbdc295d8bab20037bb7b7226fa0ffa
Yannick Lang <layaxx@gmx.de>
2021-07-15 17:33:59 +0200

    rework how secrets are handled

--- firebaseClient.ts
+++ firebaseClient.ts
@@ -1,29 +1,14 @@
 import firebaseClient from 'firebase/app'
 import 'firebase/auth'
+import config from './serviceAccountKey.json'
 
-/*
-
-Copy/paste your *client-side* Firebase credentials below. 
-
-To get these, go to the Firebase Console > open your project > Gear Icon >
-Project Settings > General > Your apps. If you haven't created a web app
-already, click the "</>" icon, name your app, and copy/paste the snippet.
-Otherwise, go to Firebase SDK Snippet > click the "Config" radio button >
-copy/paste.
-
-*/
-export const CLIENT_CONFIG = {
-  apiKey: 'AIzaSyBDa6ysrsIb7PXz2biqKd-8-OaQs-tkbwk',
-  authDomain: 'akvideo-demo.firebaseapp.com',
-  projectId: 'akvideo-demo',
-}
+export const CLIENT_CONFIG = config.client
 
 if (typeof window !== 'undefined' && !firebaseClient.apps.length) {
   firebaseClient.initializeApp(CLIENT_CONFIG)
   firebaseClient
     .auth()
     .setPersistence(firebaseClient.auth.Auth.Persistence.SESSION)
-  ;(window as any).firebase = firebaseClient
 }
 
 export { firebaseClient }
